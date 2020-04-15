d938bb1e9b2f38d111be5631ce6f504ca15f295a
hexdump0815 <hexdump0815@googlemail.com>
2020-04-15 21:48:38 +0200

    v5.6.2 + aml rename

--- misc.aml/options/enable-additional-options.sh
+++ misc.aml/options/enable-additional-options.sh
@@ -5,12 +5,12 @@ cd /compile/source/linux-stable-am
 ./scripts/config -d CONFIG_EXT2_FS
 ./scripts/config -d CONFIG_EXT3_FS
 
-for i in `cat /compile/doc/stable-am/misc.gxl/options/additional-options-yes.txt`; do
+for i in `cat /compile/doc/stable-am/misc.aml/options/additional-options-yes.txt`; do
   echo $i
   ./scripts/config -e $i
 done
 
-for i in `cat /compile/doc/stable-am/misc.gxl/options/additional-options-mod.txt`; do
+for i in `cat /compile/doc/stable-am/misc.aml/options/additional-options-mod.txt`; do
   echo $i
   ./scripts/config -m $i
 done
